9ccbe41e9c72b3a0ada7cba2a8cea322a40bc437
s65b40 <haochunwang@hotmail.com>
2017-10-16 17:26:25 +1000

    test finished of reading and writing time

---
+++ readme.md
@@ -0,0 +1,3 @@
+This is the thesis project of UQ ENGG4802.
+Author: Haochun Wang, 44948544 of UQ; 1140310109 of HIT
+This is to build a frame for elasticsearch.
--- t1.py
+++ t1.py
@@ -1,36 +1,130 @@
 # -*- coding: utf-8 -*-
-#author:Haochun Wang
+# author:Haochun Wang
 
 import time
 import sys
+import csv
 from elasticsearch import Elasticsearch
+import matplotlib.pyplot as plt
 
-
-
+reload(sys)
+sys.setdefaultencoding('utf8')
 # set up the connection with elasticsearch (this is based on the service)
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 
 class Elas_test:
     def __init__(self):
-        pass
+        self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
     def count(self):
-        num_info = es.count(index="nprobe-2017.07.01")
+        num_info = self.es.count(index="nprobe-2017.07.01")
         num_index = num_info[u'count']
-        print num_index
+        return num_index
 
-    def read(self):
-        res = es.search(index="nprobe-2017.07.01", size=1)
-        print res
+    def read_test(self, flist, read_size=100):
+        # print len(flist)
+        res = self.es.search(index="nprobe-2017.07.01", size=read_size, filter_path=flist, request_timeout=60)
+
+        # res = self.es.search(index="nprobe-2017.07.01", size=read_size, filter_path=['hits.hits._source.IN_BYTES'], request_timeout=30)
+        # res = self.es.search(index="nprobe-2017.07.01", size=read_size, filter_path=['hits.hits._source.IN_BYTES', 'hits.hits._source.@timestamp'], request_timeout=30)
+        # print res
+        return
+
+    def read_write(self, flist, read_size=100):
+        start_read = time.clock()
+        filterlst = []
+        for k in flist:
+            filterlst.append('hits.hits._source.' + k)
+        res = self.es.search(index="nprobe-2017.07.01", size=read_size, filter_path=filterlst, request_timeout=30)
+        end_read = time.clock()
+        print "For %d and " % read_size, " %d columns" % len(flist), '\t\t', \
+            "Read of %0.6f seconds" % (end_read - start_read)
+
+        start_write = time.clock()
+        with open('some%d.csv' % read_size, 'wb') as f:
+            writer = csv.writer(f)
+            writer.writerow(flist)
+            # print len(res['hits']['hits'])
+            for l in res['hits']['hits']:
+                tp = []
+                for j in flist:
+                    tp.append(l['_source'][j])
+                # print tp
+                writer.writerow(tp)
+        end_write = time.clock()
+        print "For %d and " % read_size, " %d columns" % len(flist), '\t\t', \
+            "Write of %0.6f seconds" % (end_write - start_write)
+        return
+
+    def draw_line_chart(self):
+        read_x = range(1, 30)
+        write_x = range(1, 30)
+        read_y = []
+        write_y = []
+        i = 0
+        with open('u', 'rb') as f:
+            lines = f.readlines()
+            print len(lines)
+            for line in lines:
+                print line
+                res = line.split(", ")
+                # print type(int(res[0].split(" ")[1]))
+                if int(res[0].split(" ")[1]) == 100000:
+                    if i % 2 == 0:
+                        read_y.append(float(res[2].split(" ")[2]))
+                        i += 1
+                    else:
+                        write_y.append(float(res[2].split(" ")[2]))
+                        i += 1
+        plt.plot(read_x, read_y, label='Reading time', linewidth=3, color='r', markerfacecolor='blue', markersize=12)
+        plt.plot(write_x, write_y, label='Writing time')
+        plt.xlabel('Columns')
+        plt.ylabel('Time(seconds)')
+        plt.title('R/W time test for 100000')
+        plt.legend()
+        plt.show()
+        return
 
 
 if __name__ == "__main__":
-    start = time.clock()
+    col_list = [u'L7_PROTO', u'ENGINE_ID', u'@timestamp', u'IPV4_DST_ADDR', u'UPSTREAM_TUNNEL_ID',
+                u'NPROBE_IPV4_ADDRESS', u'L4_DST_PORT', u'UNTUNNELED_IPV4_SRC_ADDR', u'DOWNSTREAM_TUNNEL_ID',
+                u'FIRST_SWITCHED', u'LAST_SWITCHED', u'UPSTREAM_SESSION_ID', u'SRC_VLAN', u'FLOW_ID',
+                u'PROTOCOL_MAP', u'PROTOCOL', u'OUT_BYTES', u'L4_SRC_PORT', u'IN_PKTS', u'IN_BYTES', u'SRC_TOS',
+                u'APPLICATION_ID', u'FLOW_PROTO_PORT', u'IPV4_SRC_ADDR', u'OUT_PKTS', u'UNTUNNELED_PROTOCOL',
+                u'DOWNSTREAM_SESSION_ID', u'@version', u'L7_PROTO_NAME']
+    es = Elas_test()
+    es.draw_line_chart()
+    # es.read_write(flist=['IN_BYTES', '@timestamp'], read_size=10)
+    ###################################################################################################################
+    ###################################################################################################################
+    '''
+    # This block is to test reading and writing time for different scales of rows and columns.
+    for i in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+        for j in xrange(1, len(col_list) + 1):
 
-    end = time.clock()
-    print "Done! With a time comsuming of %0.6f seconds" % (end - start)
-
+            tp_list = col_list[:j]
+            es.read_write(flist=tp_list, read_size=i)
+    '''
+    ###################################################################################################################
+    ###################################################################################################################
+    '''
+    # This block to test reading time for different scales of rows and columns.
+    for i in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+        for j in xrange(3, len(col_list) + 1):
+            tp_list = col_list[:j]
+            filterlst = []
+            for k in tp_list:
+                filterlst.append('hits.hits._source.' + k)
+            #print filterlst
+            start = time.clock()
+            es = Elas_test()
+            es.read_test(filterlst, read_size=i)
+            end = time.clock()
+            print "For %d and " % i, " %d columns" % j, '\t\t', "Done! With a time consumption of %0.6f seconds" % (end - start)
+    '''
+    ###################################################################################################################
+    ###################################################################################################################
 #res = es.search(index="nprobe-2017.07.01", body={"query": {"match_all": {}}})
 # res = es.search(index="nprobe-2017.07.01", size=100, filter_path=['hits.hits._id', 'hits.hits._type'])
 
@@ -44,7 +138,7 @@ for i in res['hits']['hits'][0][u'_source']:
 print type(res['hits']['hits'][0][u'_source'][u'IN_BYTES'])
 '''
 #filter_path=['hits.hits._id', 'hits.hits._type']
-#read loop pieces  csvfile java  query-field 1-2 inbyte measurement
+#read loop pieces 1000 csvfile java  query-field 1-2 inbyte measurement
 #semi
 
 
